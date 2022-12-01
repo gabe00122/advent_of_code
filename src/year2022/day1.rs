c10fbcb395496803c187af752ffc3752659e95f7
Gabriel Keith <gfk1995@gmail.com>
2022-12-01 10:14:06 -0500

    updated to u64

--- src/year2022/day1.rs
+++ src/year2022/day1.rs
@@ -1,11 +1,11 @@
 use crate::challenge_result::{ChallengeResult, ChallengeSuccess};
 
 pub fn run(input: &str) -> ChallengeResult {
-    let mut top3 = [0, 0, 0];
-    let mut local_sum = 0;
+    let mut top3: [u64; 3] = [0, 0, 0];
+    let mut local_sum: u64 = 0;
 
     for line in input.lines() {
-        if let Ok(line_value) = line.parse::<i32>() {
+        if let Ok(line_value) = line.parse::<u64>() {
             local_sum += line_value;
         } else {
             for m in &mut top3 {
