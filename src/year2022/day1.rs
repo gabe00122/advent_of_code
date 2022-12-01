8ea7451f4d7202b7bab4f430725c5d24ece41850
Gabriel Keith <gfk1995@gmail.com>
2022-12-01 10:49:22 -0500

    Improved solution

--- src/year2022/day1.rs
+++ src/year2022/day1.rs
@@ -1,16 +1,20 @@
 use crate::challenge_result::{ChallengeResult, ChallengeSuccess};
 
 pub fn run(input: &str) -> ChallengeResult {
-    let mut top3: [u64; 3] = [0, 0, 0];
+    let mut largest_sums: [u64; 3] = [0, 0, 0];
+
     let mut local_sum: u64 = 0;
 
     for line in input.lines() {
         if let Ok(line_value) = line.parse::<u64>() {
             local_sum += line_value;
         } else {
-            for m in &mut top3 {
-                if local_sum > *m {
-                    *m = local_sum;
+            for i in 0 .. largest_sums.len() {
+                if local_sum > largest_sums[i] {
+                    for j in (i + 1 .. largest_sums.len()).rev() {
+                        largest_sums[j] = largest_sums[j - 1];
+                    }
+                    largest_sums[i] = local_sum;
                     break;
                 }
             }
@@ -19,5 +23,5 @@ pub fn run(input: &str) -> ChallengeResult {
         }
     }
 
-    Ok(ChallengeSuccess::new(top3[0], top3.iter().sum()))
+    Ok(ChallengeSuccess::new(largest_sums[0], largest_sums.iter().sum()))
 }
