edd4f063e1f05321931ebce7709573e52e8fa4d6
Gabriel Keith <gabriel.keith@ncino.com>
2022-12-06 11:25:20 -0500

    Fixed day5 solution

--- src/year2022/day5.rs
+++ src/year2022/day5.rs
@@ -7,14 +7,14 @@ struct Move {
 }
 
 pub fn run(input: &str) -> ChallengeResult {
-    let lines: Vec<&str> = input.lines().filter(|line| !line.is_empty()).collect();
+    let lines: Vec<&str> = input.lines().collect();
 
     let mut stacks = parse_stacks(&lines[..8]);
-    let moves = parse_moves(&lines[9..]);
+    let moves = parse_moves(&lines[10..]);
 
-    for m in moves {
+    for m in moves.iter() {
         let new_size = stacks[m.from].len().saturating_sub(m.amount);
-        let transfer: Vec<_> = stacks[m.from].drain(..new_size).collect();
+        let transfer: Vec<_> = stacks[m.from].drain(new_size..).collect();
         stacks[m.to].extend(&transfer);
     }
 
@@ -23,32 +23,42 @@ pub fn run(input: &str) -> ChallengeResult {
     }
     println!();
 
-
     Ok(ChallengeSuccess::new(0, 0))
 }
 
-
-
 fn parse_stacks(input: &[&str]) -> Vec<Vec<char>> {
-    let lines: Vec<Vec<_>> = input.iter().rev().map(| line | line.chars().collect()).collect();
+    let lines: Vec<Vec<_>> = input
+        .iter()
+        .rev()
+        .map(|line| line.chars().skip(1).step_by(4).collect())
+        .collect();
 
-    (0..9usize).map(|i| {
-        let letter_position = 1 + i * 4;
-        lines.iter().map(|line| {
-            line[letter_position]
-        }).collect()
-    }).collect()
+    (0..9)
+        .map(|i| {
+            lines
+                .iter()
+                .map(|line| line[i])
+                .filter(|&c| c != ' ')
+                .collect()
+        })
+        .collect()
 }
 
 fn parse_moves(input: &[&str]) -> Vec<Move> {
-    input.iter().map(|line| {
-        let mut data = line.split(' ')
-            .skip(1).step_by(2).map(| x | x.parse::<usize>().unwrap());
+    input
+        .iter()
+        .map(|line| {
+            let mut data = line
+                .split(' ')
+                .skip(1)
+                .step_by(2)
+                .map(|x| x.parse::<usize>().unwrap());
 
-        Move {
-            amount: data.next().unwrap(),
-            from: data.next().unwrap() - 1,
-            to: data.next().unwrap() - 1,
-        }
-    }).collect()
+            Move {
+                amount: data.next().unwrap(),
+                from: data.next().unwrap() - 1,
+                to: data.next().unwrap() - 1,
+            }
+        })
+        .collect()
 }
