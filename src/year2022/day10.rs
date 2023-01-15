6380ada5e476ea28b6253c870d96e811480ccae0
Gabriel keith <gabe.keith@yahoo.com>
2023-01-15 12:10:44 -0500

    switch to iterator based solution

--- src/year2022/day10.rs
+++ src/year2022/day10.rs
@@ -39,14 +39,67 @@ impl FromStr for Command {
     }
 }
 
+#[derive(Debug, Clone)]
+struct Simulation<'a, I>
+where
+    I: Iterator<Item = &'a Command>
+{
+    x: i32,
+    commands: I,
+    pending_addx: Option<i32>,
+}
+
+impl<'a, I> Simulation<'a, I>
+where
+    I: Iterator<Item = &'a Command>
+{
+    fn new(commands: impl IntoIterator<IntoIter = I>) -> Self {
+        Simulation { x: 1, commands: commands.into_iter(), pending_addx: None }
+    }
+}
+
+impl<'a, I> Iterator for Simulation<'a, I>
+where
+    I: Iterator<Item = &'a Command>
+{
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(amount) = self.pending_addx {
+            let old_x = self.x;
+            self.x += amount;
+            self.pending_addx = None;
+            Some(old_x)
+        } else if let Some(&command) = self.commands.next() {
+            match command {
+                Command::NoOp => {},
+                Command::AddX(amount) => {
+                    self.pending_addx = Some(amount)
+                }
+            }
+            Some(self.x)
+        } else {
+            None
+        }
+    }
+}
+
+fn part1(commands: &[Command]) -> i32 {
+    Simulation::new(commands).zip(1..)
+        .map(|(x, cycle)| x * cycle)
+        .skip(19)
+        .step_by(40)
+        .sum()
+}
+
 pub fn run(input: &str) -> ChallengeResult {
-    let input: Vec<Command> = input.lines()
+    let commands: Vec<Command> = input.lines()
         .enumerate()
         .map(|(i, line)|
             line.parse::<Command>().map_err(|error| ParseLineError::new(i, error))
         ).collect::<Result<_, _>>()?;
 
-    println!("{:?}", &input[..7]);
+    let part1_solution = part1(&commands);
 
-    Ok(Solution::from(0, 0))
+    Ok(Solution::from(part1_solution, 0))
 }
